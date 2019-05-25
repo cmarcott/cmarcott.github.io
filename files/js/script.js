362dc49a55b749e6b3e0487246a4bc9be8d0669c
cmarcott(Desktop) <christopherlmarcotte@gmail.com>
2019-05-24 21:01:20 -0400

    Trying solution for overflow

--- files/js/script.js
+++ files/js/script.js
@@ -547,3 +547,11 @@ function splitSection() {
 	}
 	
 }
+
+jQuery('body').on('touchmove', selScrollable, function(e) {
+    // Only block default if internal div contents are large enough to scroll
+    // Warning: scrollHeight support is not universal. (https://stackoverflow.com/a/15033226/40352)
+    if(jQuery(this)[0].scrollHeight > jQuery(this).innerHeight()) {
+        e.stopPropagation();
+    }
+});
